6ab7d2a60659e2fd4adf17c286187be6b513f4e6
Amberin <amberinsyed@gmail.com>
2024-02-25 00:37:55 +0000

    Amberin's code

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const path = require('path')
 const stocks = require('./stocks')
+var cors = require('cors') //added cors library
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'static')),cors())
 
-app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+app.get('/stocks', async (req, res, next) => {
+  try {
+    const stockSymbols = await stocks.getStocks()
+    if(stockSymbols.length == 0){
+      res.send({error:'Data not found! Please check the stocks data file.'})
+    } else {
+      res.send({ stockSymbols })
+    }
+  } catch(err) {
+    next(err)
+  }
 })
+
+//   try {
+//     // Return an empty array to simulate no data
+//     // const stockSymbols = await stocks.getStocks();
+//     const stockSymbols = [];
 
 app.get('/stocks/:symbol', async (req, res) => {
   const { params: { symbol } } = req
